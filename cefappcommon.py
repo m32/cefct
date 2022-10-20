4b3d92456beedc5da6d50f9e38fa1ffd1b67a706
m32 <grzegorz.makarewicz@gmail.com>
2022-10-20 12:57:25 +0200

    handlers

--- cefappcommon.py
+++ cefappcommon.py
@@ -1,7 +1,44 @@
 import ctypes as ct
 from cefct import libcef as cef
 
-class LifeSpanHandler(cef.cef_life_span_handler_t):
+class CefLoadHandler(cef.cef_load_handler_t):
+    def _on_loading_state_change(self, this, browser, isLoading, canGoBack, canGoForward):
+        print('CefLoadHandler.on_loading_state_change(browser, {}, {}, {})'.format(
+            isLoading, canGoBack, canGoForward
+        ), flush=True)
+    def _on_load_start(self, this, browser, frame, transition_type):
+        print('CefLoadHandler.on_load_start(browser, frame, {})'.format(
+            transition_type
+        ), flush=True)
+    def _on_load_end(self, this, browser, frame, httpStatusCode):
+        print('CefLoadHandler.on_load_end(browser, frame, {})'.format(
+            httpStatusCode
+        ), flush=True)
+    def _on_load_error(self, this, browser, frame, errorCode, errorText, failedUrl):
+        print('CefLoadHandler.on_load_error(browser, frame, {}, {}, {})'.format(
+            errorCode, errorText, failedUrl
+        ), flush=True)
+
+class CefBrowserProcessHandler(cef.cef_browser_process_handler_t):
+    def _on_context_initialized(self, this):
+        print('CefBrowserProcessHandler.on_context_initialized', flush=True)
+
+    def _on_before_child_process_launch(self, this, command_line):
+        print('CefBrowserProcessHandler.on_before_child_process_launch', flush=True)
+        cl = command_line.contents
+        print('\tcl.valid', cl.is_valid(cl))
+        if 0 and cl.is_valid(cl):
+            s = cl.get_command_line_string(command_line)
+            print('\tcl.string', s)
+
+    def _on_schedule_message_pump_work(self, this, delay_ms):
+        pass
+        #print('CefBrowserProcessHandler.on_before_child_process_launch', Flush=True)
+
+    def _get_default_client(self, this):
+        print('CefBrowserProcessHandler.get_default_client', flush=True)
+
+class CefLifeSpanHandler(cef.cef_life_span_handler_t):
     def _on_before_popup(self, *args):
         print('LifeSpanHandler.OnBeforePopup')
         return 0
@@ -15,45 +52,14 @@ class LifeSpanHandler(cef.cef_life_span_handler_t):
         print('LifeSpanHandler.OnBeforeClose')
         cef.cef_quit_message_loop()
 
-class LoadHandler(cef.cef_load_handler_t):
-    def _on_loading_state_change(self, this, browser, a, b, c):
-        print(f'OnLoadingStateChange(self, this, browser, {a}, {b}, {c})')
-    def _on_load_start(self, this, browser, frame, ttype):
-        print(f'OnLoadStart(self, this, browser, frame, {ttype:08x})')
-    def _on_load_end(self, this, browser, frame, a):
-        print(f'OnLoadEnd(self, this, browser, frame, {a})')
-    def _on_load_error(self, this, browser, frame, a, b, c):
-        print(f'OnLoadError(self, this, browser, frame, {a}, {b}, {c})')
-
-
 class Client(cef.cef_client_t):
     def __init__(self):
         super().__init__()
-        self.life_span_handler = LifeSpanHandler()
-        self.load_handler = LoadHandler()
+        self.life_span_handler = CefLifeSpanHandler()
+        self.load_handler = CefLoadHandler()
     def _get_life_span_handler(self, *args):
-        print('GetLifeSpanHandler')
         ret = ct.addressof(self.life_span_handler)
         return ret
     def _get_load_handler(self, *args):
-        print('GetLoadHandler')
         ret = ct.addressof(self.load_handler)
         return ret
-
-class BrowserProcessHandler(cef.cef_browser_process_handler_t):
-    def _on_context_initialized(self, this):
-        print('BrowserProcessHandler.OnContextInitialized(self, this)')
-        pass
-
-    def _on_before_child_process_launch(self, this, commandLine):
-        cls = commandLine.contents._get_command_line_string(commandLine).contents.ToString()
-        print(f'BrowserProcessHandler.OnBeforeChildProcessLaunch(self, this, {cls})')
-        pass
-
-    def _on_schedule_message_pump_work(self, this, delayMs):
-        print(f'BrowserProcessHandler.OnScheduleMessagePumpWork(self, this, {delayMs})')
-        pass
-
-    def _get_default_client(self, this):
-        print(f'BrowserProcessHandler.GetDefaultClient(self, this)')
-        pass
