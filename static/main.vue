b0319ba4b9b11b2c92c034884331ba0202a20730
m32 <grzegorz.makarewicz@gmail.com>
2022-10-21 01:00:23 +0200

    flask & vue & wxPython

---
+++ static/main.vue
@@ -0,0 +1,24 @@
+<template>
+    <mycomponent
+        :name="'circle' + index % 2"
+    />
+</template>
+
+<script>
+import mycomponent from './myComponent.vue'
+import { ref } from 'vue'
+
+export default {
+    components: {
+        mycomponent
+    },
+    setup() {
+        const index = ref(0);
+        setInterval(() => index.value++, 1000);
+
+        return {
+            index,
+        }
+    },
+}
+</script>
---
+++ static/myComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <span v-html="svg" />
+</template>
+<script>
+import { ref, watch } from 'vue'
+
+class test {
+
+}
+
+console.log(test)
+
+function asyncToRef(callback) {
+    const val = ref();
+    watch(() => callback(), promise => promise.then(value => val.value = value), { immediate: true });
+    return val;
+}
+
+export default {
+    props: {
+        name: String
+    },
+    setup(props) {
+        return {
+            svg: asyncToRef(() => import('./' + (props.name ?? '') + '.svg')),
+        }
+    }
+}
+</script>
